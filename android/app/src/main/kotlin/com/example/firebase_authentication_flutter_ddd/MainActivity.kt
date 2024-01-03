aa81151d1c76868a2ea45871346f7bc3da9f1c86
Vetrichelvan <pythonhubdev@gmail.com>
2024-01-03 12:25:06 +0530

    feat(packages): updated packages

---
+++ android/app/src/main/kotlin/com/example/firebase_authentication_flutter_ddd/MainActivity.kt
@@ -0,0 +1,6 @@
+package com.example.firebase_authentication_flutter_ddd
+
+import io.flutter.embedding.android.FlutterActivity
+
+class MainActivity: FlutterActivity() {
+}
--- android/app/src/main/kotlin/dev/pythonhub/firebase_auth_flutter_ddd/MainActivity.kt
+++
@@ -1,6 +0,0 @@
-package dev.pythonhub.firebase_auth_flutter_ddd
-
-import io.flutter.embedding.android.FlutterActivity
-
-class MainActivity : FlutterActivity() {
-}
